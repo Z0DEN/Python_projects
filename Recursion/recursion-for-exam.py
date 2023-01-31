f4cb0109aa353f4492eb995a31949204e015a4cc
BlesK <109669748+Z0DEN@users.noreply.github.com>
2023-01-31 15:25:11 +0700

    1-st-com

--- Recursion/recursion-for-exam.py
+++ Recursion/recursion-for-exam.py
@@ -1,1 +1,14 @@
+def f(n):
+    if n <= 3:
+        return n
+    if n > 3:
+        if n % 2 == 0:
+            return 2 * n * n + f(n-1)
+        return n * n* n + n + f(n-1)
 
+
+t = 0
+for n in range (1, 1001):
+  if f(n) < 10**3:
+    t += 1
+print(t)
