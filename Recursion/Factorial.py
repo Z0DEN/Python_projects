1cb95f249d873ce0fb622998975f55f01b64bc9b
Z0DEN <maksarestov2000@gmail.com>
2023-01-31 11:23:57 +0700

    factorial

---
+++ Recursion/Factorial.py
@@ -0,0 +1,9 @@
+from art import *
+Art = text2art("factorial", font='block', chr_ignore=True)
+def factorial(n)
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+
+print(factorial(input("введите число")))
