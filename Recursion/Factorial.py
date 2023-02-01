053ac77520d60579c3cf4675775a0fac7b2c376e
BlesK <109669748+Z0DEN@users.noreply.github.com>
2023-02-01 15:11:42 +0700

    fix-def-factorial

--- Recursion/Factorial.py
+++ Recursion/Factorial.py
@@ -1,6 +1,8 @@
 from art import *
 Art = text2art("factorial", font='block', chr_ignore=True)
-def factorial(n)
+
+
+def factorial(n):
     if n == 1:
         return 1
     return n * factorial(n-1)
