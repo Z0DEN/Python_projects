d000bc8da353f1f6e078dda80b4d4fe5c102049b
Z0DEN <maksarestov2000@gmail.com>
2023-02-05 19:35:16 +0700

    rename

--- Recursion/123.py
+++
@@ -1,7 +0,0 @@
-from colorama import init
-from termcolor import colored
-init()
-print (colored('{0:{width}}'.format('', width = 500)))
-print (colored('{0:{width}}'.format('Hello World!', width = 500), 'green'))
-print (colored('{0:{width}}'.format('Hi!', width = 0), 'green'))
-print (colored('{0:{width}}'.format('', width = 500)))
