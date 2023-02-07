17b91b7ee542cadccd8cf525f7919b32a21a31cc
Z0DEN <maksarestov2000@gmail.com>
2023-02-07 22:28:43 +0700

    comm

--- Recursion/Factorial.py
+++ Recursion/Factorial.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from art import *
 from colorama import init, Fore
 from colorama import Back
@@ -9,12 +10,24 @@ Art = tprint('factorial')
 
 
 def factorial(n):
+=======
+#from art import *
+#Art = tprint("recursion")
+n = input()
+
+
+def f(n):
+>>>>>>> 34e3262 (comm)
     if n == 1:
         return 1
-    return n * factorial(n-1)
+    return n * f(n-1)
 
 
+<<<<<<< HEAD
 t = int(input(Fore.GREEN + Style.BRIGHT + 'введите число: '))
 print (colored('{0:{width}}'.format('', width = 200)))
 print(Fore.BLUE + Style.BRIGHT + 'факториал', colored('{0:{width}}'.format(t, width = 0), 'red'),Fore.BLUE + Style.BRIGHT + '=', colored('{0:{width}}'.format(factorial(t), width = 0), 'red'))
 print (colored('{0:{width}}'.format('', width = 200)))
+=======
+print(f(n))
+>>>>>>> 34e3262 (comm)
