dc2d9706ea35861313b4856b0e418bbadba87e80
BlesK <109669748+Z0DEN@users.noreply.github.com>
2023-02-07 14:54:35 +0700

    rename+19-taask

--- Recursion/task-number-16-(19).py
+++ Recursion/task-number-16-(19).py
@@ -1,10 +1,10 @@
 def f(n):
-    if n > 30:
+    if n <= 18:
         return n*n+3*n+5
-    if n <= 30 and n % 2 == 0:
-        return 2 * f(n+1) + f(n+4)
-    if n <= 30 and n % 2 == 1:
-        return f(n+2) + 3*f(n+5)
+    if n > 18 and n % 3 == 0:
+        return (n // 3) * f(n // 3) + n - 12
+    if n > 18 and n % 3 != 0:
+        return f(n-1) + n * n + 5
 
 
 #   k --- количество нулей в числе
@@ -13,13 +13,15 @@ def f(n):
 
 
 m = 0
-for n in range(1, 1001):
+for n in range(1, 801):
     a = abs(f(n))
     k = 0
+    t = 0
     while a > 0:
-        if a % 10 == 0:
+        if ((a % 10) % 2) == 0:
             k += 1
         a //= 10
-    if k > 2:
+        t += 1
+    if k == t:
         m += 1
 print(m)
