39ff6b643db6a53df3769f7304847c1182eb03b0
Z0DEN <maksarestov2000@gmail.com>
2023-02-19 14:34:26 +0700

    super-mega-commit

--- Excel/proverka.py
+++ Excel/proverka.py
@@ -1,13 +1,24 @@
-from PIL import Image
+import openpyxl
+from PIL import Image, ImageDraw
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+import numpy as np
 
-im = Image.open("/home/blesk/GitHub/Python_projects/cropped_image.jpg")
-pixels = im.load() # список с пикселями
-x, y = im.size # ширина (x) и высота (y) изображения
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
 
+im = Image.new('RGB', (20, 20), (255, 255, 255))
+draw = ImageDraw.Draw(im)
 
-for i in range(x):  
-    for j in range(y):
-        r, g, b = pixels[i, j]
-        rgb = f"{r}, {g}, {b}"
-        print('#' + ''.join([f"{int(i):02x}" for i in rgb.split(',')]))
-        print(r, g, b)
+workbook = openpyxl.load_workbook('/home/blesk/GitHub/Python_projects/img-to-xlsx.xlsx')
+sheets_list = workbook.sheetnames
+x = 0
+y = 0
+for cell_letter in alphabet:
+    for cell_number in range(1, 21):
+            cell_fill = sheet_active[f'{cell_letter}{cell_number}'].fill.start_color.index #Получаем цвет ячейки
+            cell_ch ='#' + cell_fill[2:8]
+            draw.rectangle((x, y, x, y), fill=f'{cell_ch}')
+            x += 1
+            y += 1
+im.show()
