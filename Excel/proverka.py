e1340654544b205bc9f4a09dda99dae47cf3b6a5
Z0DEN <maksarestov2000@gmail.com>
2023-02-27 00:24:04 +0700

    done

--- Excel/proverka.py
+++ Excel/proverka.py
@@ -1,5 +1,14 @@
-a = 5
-b = 10
-if a/b == 0.5:
-    print(a/b)
-else:print('xui')
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+ws.title = 'RowColDimension'
+ws.column_dimensions['B'].width = 30
+ws.column_dimensions['D'].width = 50
+ws.row_dimensions[5].height = 50
+ws['B5'] = 'ширина = 30, высота = 50'
+ws['D5'] = 'ширина = 50, высота = 50'
+from openpyxl.styles import Alignment
+for cell in ws[5]:
+    if cell.value:
+        cell.alignment = Alignment(horizontal="center", vertical="center")
+        wb.save('width-height.xlsx')
