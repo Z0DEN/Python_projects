bee5dcf29d46f15f08828180d2fde6e6e892a758
Z0DEN <maksarestov2000@gmail.com>
2023-02-27 00:23:50 +0700

    done

--- Excel/img-to-xlsx.py
+++ Excel/img-to-xlsx.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 def crop(file_name):
     filename = file_name
@@ -9,15 +10,15 @@ def crop(file_name):
     print(img.size)
     cropped_img = img.crop((80, 100, 180, 200))
     print(cropped_img.size)
-    cropped_img.save("cropped_image.jpg")
+    cropped_img.save("cropped_image.png")
 
-#crop('/home/blesk/GitHub/Python_projects/Excel/test.jpg')
+#crop('/home/blesk/GitHub/Python_projects/Excel/test.png')
 
 wb = Workbook()
 ws = wb.active
 ws1 = wb.create_sheet("Mysheet")
 
-im = Image.open("/home/blesk/GitHub/Python_projects/Excel/test.jpg")
+im = Image.open("/home/blesk/GitHub/Python_projects/Excel/test.png")
 pixels = im.load() # список с пикселями
 x, y = im.size # ширина (x) и высота (y) изображения
 
@@ -27,8 +28,12 @@ deg = 0.05
 degree = 5
 space = '------------------------'
 k = 1
+g = 0
 for i in range(x):
     for j in range(y):
+        g += 1
+        letter = get_column_letter(g)
+        ws.column_dimensions[f'{letter}'].width = 3
         r, g, b = pixels[i, j]
         rgb = f"{r}, {g}, {b}"
         hex = (''.join([f"{int(i):02x}" for i in rgb.split(',')]))
